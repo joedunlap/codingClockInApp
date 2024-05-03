a9429b683a22bae1e63b7dd67ed44d54c102de09
joedunlap <joedunlap26@gmail.com>
2024-05-03 17:50:16 -0500

    added schema and model file

---
+++ models/model.js
@@ -0,0 +1,60 @@
+import { db } from '../lib/database.js';
+import Constants from '../lib/constants.js';
+
+export default class TimeLogModel {
+  static getTimeLogs = async () => {
+    try {
+      const timeLogs = await db.getTimeLogsCollection().find({}, { projection: Constants.DEFAULT_PROJECTION }).toArray();
+      return timeLogs;
+    } catch (error) {
+      console.error('Error fetching time logs:', error);
+      throw new Error('Failed to fetch time logs');
+    }
+  };
+
+  static createTimeLog = async (newTimeLog) => {
+    try {
+      await db.getTimeLogsCollection().insertOne(newTimeLog);
+      return newTimeLog;
+    } catch (error) {
+      console.error('Error creating time log:', error);
+      throw new Error('Failed to create time log');
+    }
+  };
+
+  static getTimeLogById = async (id) => {
+    try {
+      const timeLog = await db.getTimeLogsCollection().findOne({ id }, { projection: Constants.DEFAULT_PROJECTION });
+      return timeLog;
+    } catch (error) {
+      console.error(`Error fetching time log with ID ${id}:`, error);
+      throw new Error('Failed to fetch time log');
+    }
+  };
+
+  static deleteTimeLog = async (id) => {
+    try {
+      const result = await db.getTimeLogsCollection().deleteOne({ id });
+      if (result.deletedCount === 1) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error(`Error deleting time log with ID ${id}:`, error);
+      throw new Error('Failed to delete time log');
+    }
+  };
+
+  static updateTimeLog = async (id, updatedTimeLog) => {
+    try {
+      const result = await db.getTimeLogsCollection().updateOne({ id }, { $set: updatedTimeLog });
+      if (result.matchedCount === 1) {
+        return updatedTimeLog;
+      }
+      return false;
+    } catch (error) {
+      console.error(`Error updating time log with ID ${id}:`, error);
+      throw new Error('Failed to update time log');
+    }
+  };
+}
